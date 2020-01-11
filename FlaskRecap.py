7c0261bbf099a89fda030962d343592517738d04
SammyLev <sammy.murray@gmail.com>
2020-01-11 12:24:16 -0800

    Process headers

--- FlaskRecap.py
+++ FlaskRecap.py
@@ -1,4 +1,27 @@
 from flask import Flask, request, jsonify, abort
+from functools import wraps
+
+def get_token_auth_header():
+    if 'Authorization' not in request.headers:
+        abort(401)
+
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower() != 'bearer':
+        abort(401)
+
+    return header_parts[0]
+
+def requires_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        jwt = get_token_auth_header()
+        return f(jwt,*args,**kwargs)
+    return wrapper
+
 
 app = Flask(__name__)
 
@@ -11,6 +34,13 @@ greetings = {
             'he': 'שלום',
             'ja': 'こんにちは'
             }
+
+@app.route('/headers')
+@requires_auth
+def headers(jwt):
+    print(jwt)
+
+    return 'not implemented'
 
 @app.route('/greeting', methods=['GET'])
 def greeting_all():
